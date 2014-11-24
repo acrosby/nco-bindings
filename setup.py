75511f5fca7536de50544458a24ee1a4f9021744
Alexander Crosby <crosbyar@gmail.com>
2014-11-24 11:47:09 -0500

    change to setup.py

--- setup.py
+++ setup.py
@@ -1,21 +1,20 @@
 #!/usr/bin/env python
 from setuptools import setup, find_packages
 
-setup (name = 'nco',
-  version = '0.0beta',
-  author = "Joe Hamman",
-  author_email= "jhamman@hydro.washington.edu",
-  license = "GPLv2",
-  description = """python bindings to NCO""",
-  py_modules = ["nco"],
-  url = "https://raw2.github.com/jhamman/nco-bindings/",
-  download_url = "https://raw2.github.com/jhamman/nco-bindings/tarball/0.1",
-  keywords = ['netcdf', 'climate'],
-  classifiers = [
-        "Development Status :: 2 - Pre-Alpha",
-        "Topic :: Utilities",
-        "Operating System :: POSIX",
-        "Programming Language :: Python",
-    ],
-  packages = find_packages(),
+setup(  name          = 'nco',
+        version       = '0.0beta',
+        author        = "Joe Hamman",
+        author_email  = "jhamman@hydro.washington.edu",
+        license       = "GPLv2",
+        description   = """python bindings to NCO""",
+        url           = "https://raw2.github.com/jhamman/nco-bindings/",
+        download_url  = "https://raw2.github.com/jhamman/nco-bindings/tarball/0.1",
+        keywords      = ['netcdf', 'climate'],
+        classifiers   = [
+                          "Development Status :: 2 - Pre-Alpha",
+                          "Topic :: Utilities",
+                          "Operating System :: POSIX",
+                          "Programming Language :: Python",
+                        ],
+        packages      = find_packages(),
   )
